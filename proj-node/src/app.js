21da1a97ba1aab8347af1cd232ef29cb4eb939cd
Bianca Tomie <82179770+biatomie@users.noreply.github.com>
2023-04-03 20:51:27 -0300

    update banco

--- proj-node/src/app.js
+++ proj-node/src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())//interpretar a resposta em json
+
 const imoveis = [
-    {id: 1, "endereco": "jabaquara"},
-    {id: 2, "endereco": "saude"}
+    {id: 1, "end": "jabaquara"},
+    {id: 2, "end": "saude"}
 ]
 
 app.get('/', (req, res) => {
@@ -12,9 +14,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/cadastro', (req, res) => {
-    res.status(200).send('Cadastro de Imoveis', (req, res) => {
-        res.status(200).json(imoveis)    
-    });
+    res.status(200).json(imoveis);
 })
+
+
+app.get('/cadastro/:id', (req, res) => {
+    let index = buscaImovel(req.params.id); //parametro id da requisição
+    res.json(imoveis[index]);
+})
+
+app.post('/cadastro', (req, res) => {
+    imoveis.push(req.body);
+    res.status(201).send("Imóvel cadastrado com sucesso")
+})
+
+app.put('/cadastro/:id', (req, res) => {
+    let index = buscaImovel(req.params.id); //parametro id da requisição
+    imoveis[index].end = req.body.end;
+    res.json(imoveis);
+})
+
+function buscaImovel(id) {
+    return imoveis.findIndex(imovel => imovel.id == id)
+}
 
 export default app
